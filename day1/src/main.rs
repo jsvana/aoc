5518f57d33a2adf2720bb1a8d013b8877545424f
Jay Vana <jaysvana@gmail.com>
2020-11-30 21:06:58 -0800

    Day 1 solution maybe? Auth is down so I can't check.

---
+++ day1/src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::BTreeSet;
+
+use anyhow::Result;
+
+fn read_numbers(filename: &str) -> Result<Vec<i32>> {
+    let contents = std::fs::read_to_string(filename)?;
+
+    let mut numbers = Vec::new();
+    for line in contents.split("\n") {
+        if line.is_empty() {
+            continue;
+        }
+
+        numbers.push(line.parse()?);
+    }
+
+    Ok(numbers)
+}
+
+fn main() -> Result<()> {
+    // TODO(jsvana): make this a commandline argument
+    let numbers = read_numbers("test.txt")?;
+
+    let mut missing = Vec::new();
+    for number in numbers.iter() {
+        missing.push(2020 - number);
+    }
+
+    let all: BTreeSet<_> = numbers.iter().collect();
+    for (i, missing_number) in missing.into_iter().enumerate() {
+        if all.contains(&missing_number) {
+            println!("{}", missing_number * numbers[i]);
+            break;
+        }
+    }
+
+    Ok(())
+}
---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/jsvana/projects/aoc_2020/day1/target/debug/day1: /home/jsvana/projects/aoc_2020/day1/src/main.rs
