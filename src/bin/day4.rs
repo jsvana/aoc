c8decd194e906f17f9543e495ae8a3f524626c52
Jay Vana <jaysvana@gmail.com>
2020-12-03 20:43:12 -0800

    Move to separate binaries instead

---
+++ src/bin/day4.rs
@@ -0,0 +1,12 @@
+use anyhow::{format_err, Result};
+use structopt::StructOpt;
+
+use aoc_2020::{read_lines, Args};
+
+fn main() -> Result<()> {
+    let args = Args::from_args();
+
+    //let contents = std::fs::read_to_string(args.filename)?;
+
+    Ok(())
+}
