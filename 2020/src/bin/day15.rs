cf4bd3f642cd8711e8906967662471fb3d3221b8
Jay Vana <jaysvana@gmail.com>
2020-12-14 21:39:41 -0800

    minimal cleanup

--- 2020/src/bin/day15.rs
+++ 2020/src/bin/day15.rs
@@ -10,7 +10,6 @@ fn main() -> Result<()> {
 
     let mut last_number = numbers[0];
     for i in 1..=30000000 {
-        //for i in 1..=9 {
         if i - 1 < numbers.len() {
             let number = numbers[i - 1];
             said.entry(number).or_insert(Vec::new()).push(i);
@@ -20,27 +19,20 @@ fn main() -> Result<()> {
             continue;
         }
 
-        // Generate next number
-        match said_count.get(&last_number) {
-            Some(count) => {
-                if *count == 1 {
-                    said.entry(0).or_insert(Vec::new()).push(i);
-                    *said_count.entry(0).or_insert(0) += 1;
-                    last_number = 0;
-                } else {
-                    let turns = said
-                        .get(&last_number)
-                        .ok_or_else(|| format_err!("num {} missing", last_number))?;
+        if let Some(count) = said_count.get(&last_number) {
+            if *count == 1 {
+                said.entry(0).or_insert(Vec::new()).push(i);
+                *said_count.entry(0).or_insert(0) += 1;
+                last_number = 0;
+            } else {
+                let turns = said
+                    .get(&last_number)
+                    .ok_or_else(|| format_err!("num {} missing", last_number))?;
 
-                    let difference = turns[turns.len() - 1] - turns[turns.len() - 2];
-                    //println!("  turn {} - last turn {} = {}", i, turn, difference);
-                    said.entry(difference as i64).or_insert(Vec::new()).push(i);
-                    *said_count.entry(difference as i64).or_insert(0) += 1;
-                    last_number = difference as i64;
-                }
-            }
-            None => {
-                todo!();
+                let difference = turns[turns.len() - 1] - turns[turns.len() - 2];
+                said.entry(difference as i64).or_insert(Vec::new()).push(i);
+                *said_count.entry(difference as i64).or_insert(0) += 1;
+                last_number = difference as i64;
             }
         }
 
